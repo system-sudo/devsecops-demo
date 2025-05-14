591c8e5b799299415a5ef747017aea5e18883da8
system-sudo <systemtesting48@gmail.com>
2025-05-14 18:49:03 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/tic-tac-toe/', // 👈 This is the key line
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
